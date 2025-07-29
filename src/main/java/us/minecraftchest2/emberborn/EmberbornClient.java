edb93135b3cc8167f4f0737e8b0ee1339d30c7ea
dragonruler1000 <github@me.minecraftchest2.us>
2025-07-29 13:38:15 -0500

    Add EmberbornClient class and update mod description and license

---
+++ src/main/java/us/minecraftchest2/emberborn/EmberbornClient.java
@@ -0,0 +1,10 @@
+package us.minecraftchest2.emberborn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EmberbornClient  implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
