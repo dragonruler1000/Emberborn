96cdc255195b46db289eb4702301e6c403f14dcc
dragonruler1000 <github@me.minecraftchest2.us>
2025-07-29 09:17:39 -0500

    Initial commit

---
+++ src/main/java/us/minecraftchest2/emberborn/EmberbornDataGenerator.java
@@ -0,0 +1,11 @@
+package us.minecraftchest2.emberborn;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EmberbornDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
